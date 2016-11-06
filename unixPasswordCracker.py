69db3a04078d5ac2b3921b5d6dfea923ffaf7e3a
Edima Udo <edimaudo@gmail.com>
2016-11-06 17:10:16 -0500

    unixPasswordCracker

---
+++ unixPasswordCracker.py
@@ -0,0 +1,28 @@
+import crypt
+
+def testPass(cryptPass):
+
+    salt = cryptPass[0:2]
+    dictfile = open('dictionary.txt', 'r')
+    for word in dictfile.readlines():
+        word = word.strip('\n')
+    cryptword = crypt.crypt(word, salt)
+    if cryptword == cryptPass:
+        print "[+] Found Password: " + word + "\n"
+    return
+    print "[-] Password Not Found.\n"
+    return
+
+
+def main():
+
+    passFile = open('passwords.txt')
+    for line in passFile.readlines():
+        if ":" in line:
+            user = line.split(':')[0]
+    cryptPass = line.split(':')[1].strip(' ')
+    print "[*] Cracking Password For: " + user
+    testPass(cryptPass)
+
+if __name__ == "__main__":
+    main()
